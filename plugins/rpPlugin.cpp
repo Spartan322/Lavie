1d4419050fd0711bec03cff585c7ce0aea9b2602
Geroge <Spartan322@live.com>
2014-06-20 11:50:03 -0400

    Update rpPlugin.cpp

--- plugins/rpPlugin.cpp
+++ plugins/rpPlugin.cpp
@@ -1,22 +1,37 @@
 #include "rpPlugin.h"
 
+#define RP_NAME_SAY 0
+
 int rp::handleCommand(string nick, string channel, vector<string> words)
 {
 		string reply = "";
 		if(nick != "")
 		{
-				reply += nick + ": ";
+				reply += (RP_SAY_NAME == 1) ? nick + ": " : channel + " GameMaster: ";
 		}
+		/*Roll Command*/
 		if(words.at(0) == "roll")
 		{
-			if(words.length < 3)
+			string text;
+			if(words.size() < 3)
 			{
-				rpActions::roll(words.at(1), words.at(2));
+				text = rpActions::roll(words.at(1), words.at(2));
 				return 1;
 			}
 			else
 			{
-				rpActions::roll(words.at(1), words.at(2), words.at(3), words;
+				vector<string> swords;
+				for(int i = 2; i < words.size(); i++)
+				{
+					swords.at(i-2) = words.at(i);
+				}
+				text = rpActions::roll(words.at(1), words.at(2), words.at(3), swords);
+				~swords();
 			}
+			reply += text;
+			return 1;
 		}
+		/*End Roll Command*/
 }
+
+int rp
