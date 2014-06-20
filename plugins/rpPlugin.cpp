769aed17632551ce233978f7d3c2e864c15973b6
Geroge <Spartan322@live.com>
2014-06-20 10:08:35 -0400

    Update rpPlugin.cpp

--- plugins/rpPlugin.cpp
+++ plugins/rpPlugin.cpp
@@ -2,5 +2,21 @@
 
 int rp::handleCommand(string nick, string channel, vector<string> words)
 {
-
+		string reply = "";
+		if(nick != "")
+		{
+				reply += nick + ": ";
+		}
+		if(words.at(0) == "roll")
+		{
+			if(words.length < 3)
+			{
+				rpActions::roll(words.at(1), words.at(2));
+				return 1;
+			}
+			else
+			{
+				rpActions::roll(words.at(1), words.at(2), words.at(3), words;
+			}
+		}
 }
